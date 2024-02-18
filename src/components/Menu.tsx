b48f8b02aa6c4059d283d9ad41191a19d15ad10a
Wiktor Jakubowski <qbagency@gmail.com>
2024-02-18 16:05:48 +0100

    Fix alignment

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,7 +11,7 @@ export default function Menu() {
           <li key={category.categoryId}>
             <h3
               id={category.categoryId.toString()}
-              className="pl-12 pt-20 text-lg font-semibold uppercase dark:text-white"
+              className="pl-4 pt-20 text-lg font-semibold uppercase dark:text-white lg:pl-12"
             >
               {category.title}
             </h3>
